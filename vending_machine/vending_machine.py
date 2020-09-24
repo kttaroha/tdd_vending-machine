81d48b39575bd9f629cb69d0cd27fd656f924360
kttaroha <kthamano1994@gmail.com>
2020-09-24 13:29:06 +0900

    add __init__

--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,3 +1,6 @@
 class VendingMachine:
+    def __init__(self):
+        self.coins = {10: 0, 50: 0, 100: 0, 500: 0, 1000: 0}
+
     def insert(self, coin):
-        pass
+        self.coins[coin] += 1
