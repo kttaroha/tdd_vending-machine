1d6a53d5ce178261c3a5a11311976a1aff4f4b21
kttaroha <kthamano1994@gmail.com>
2020-09-24 13:39:50 +0900

    modify insert

--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -3,4 +3,8 @@ class VendingMachine:
         self.coins = {10: 0, 50: 0, 100: 0, 500: 0, 1000: 0}
 
     def insert(self, coin):
-        self.coins[coin] += 1
+        if type(coin) == int:
+            self.coins[coin] += 1
+        elif type(coin) == list:
+            for c in coin:
+                self.coins[c] += 1
