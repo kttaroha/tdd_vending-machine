84314621a0dfc3df8f54fc6ccd807635713813a6
zhouhua <1915671841@qq.com>
2020-09-24 12:50:38 +0900

    add insert coins test

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -1,2 +1,11 @@
+from vending_machine.vending_machine import VendingMachine
+
+
 def test_huga():
     assert True
+
+
+def test_insert_10coin():
+    vending_machine = VendingMachine()
+    coins = 10
+    vending_machine.insert(coins)
