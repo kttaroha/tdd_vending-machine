9e104fd9d676d014d4c87ef877ac0fb8599ad8c6
kttaroha <kthamano1994@gmail.com>
2020-09-24 13:26:57 +0900

    add test_check_coins

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -9,3 +9,13 @@ def test_insert_10coin():
     vending_machine = VendingMachine()
     coins = 10
     vending_machine.insert(coins)
+
+
+def test_check_coins():
+    vending_machine = VendingMachine()
+    insert_coins = 10
+    vending_machine.insert(insert_coins)
+    actual = vending_machine.coins
+    expected = {10: 1, 50: 0, 100: 0, 500: 0, 1000: 0}
+
+    assert actual == expected
