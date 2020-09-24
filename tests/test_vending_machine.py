cf75da361bbb9d6eccefaee84532f9a8d8c1edb9
kttaroha <kthamano1994@gmail.com>
2020-09-24 13:36:59 +0900

    add test_check_multiple_insert

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -15,7 +15,19 @@ def test_check_coins():
     vending_machine = VendingMachine()
     insert_coins = 10
     vending_machine.insert(insert_coins)
+
     actual = vending_machine.coins
     expected = {10: 1, 50: 0, 100: 0, 500: 0, 1000: 0}
+
+    assert actual == expected
+
+
+def test_check_multiple_insert():
+    vending_machine = VendingMachine()
+    insert_coins = [10, 50, 100, 500, 1000]
+    vending_machine.insert(insert_coins)
+
+    actual = vending_machine.coins
+    expected = {10: 1, 50: 1, 100: 1, 500: 1, 1000: 1}
 
     assert actual == expected
