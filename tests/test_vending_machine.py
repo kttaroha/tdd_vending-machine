3c88e5246c2c017f10f3fb02ecc445ab96e28922
masahh <masaru.yoshioka@ariseanalytics.com>
2020-10-01 14:52:49 +0900

    refactor & add method for getting juice info

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -1,29 +1,35 @@
 from vending_machine.vending_machine import VendingMachine
 
 
-def test_check_money():
-    vending_machine = VendingMachine()
-    insert_money = 10
-    vending_machine.insert(insert_money)
-
-    actual = vending_machine.money_box
-    expected = {10: 1, 50: 0, 100: 0, 500: 0, 1000: 0}
-
-    assert actual == expected
-
-
 def test_check_multiple_insert():
+    """複数のお金が挿入できるか"""
+
     vending_machine = VendingMachine()
     insert_money = [10, 50, 100, 500, 1000]
-    vending_machine.insert(insert_money)
+    actual_insert = vending_machine.insert(insert_money)
 
-    actual = vending_machine.money_box
+    assert actual_insert == []
+
+    actual_money_box = vending_machine.money_box
     expected = {10: 1, 50: 1, 100: 1, 500: 1, 1000: 1}
 
+    assert actual_money_box == expected
+
+
+def test_insert_invalid_money():
+    """無効なお金が投入された物がそのまま返却されるか"""
+
+    vending_machine = VendingMachine()
+    insert_money = [1, 5, 10000, 3]
+
+    actual = vending_machine.insert(insert_money)
+    expected = insert_money
     assert actual == expected
 
 
 def test_get_amount():
+    """自販機に投入した金額の総計が正しく出力されるか"""
+
     vending_machine = VendingMachine()
     insert_money = [10, 50, 100, 500, 1000]
     vending_machine.insert(insert_money)
@@ -35,6 +41,8 @@ def test_get_amount():
 
 
 def test_refund_money():
+    """自販機に入っているお金が正しく返金されるか"""
+
     vending_machine = VendingMachine()
     insert_money = [10, 50, 100, 500, 1000]
     vending_machine.insert(insert_money)
@@ -49,12 +57,13 @@ def test_refund_money():
 
     assert actual_amount == expected_amount
 
-def test_insert_invalid_money():
+
+def test_get_juice_info():
+    """自販機で管理されているジュースの情報を取得する"""
+
     vending_machine = VendingMachine()
-    insert_money = [1, 5, 10000, 3]
-    
-    actual = vending_machine.insert(insert_money)
-    expected = insert_money
+
+    actual = vending_machine.get_juice_info()
+    expected = {"coke": {"price": 120, "num": 5}}
+
     assert actual == expected
-
-
--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,34 +1,34 @@
+from typing import List, Dict
+
+
 class VendingMachine:
     def __init__(self):
         self.money_box = {10: 0, 50: 0, 100: 0, 500: 0, 1000: 0}
+        self.juice_box = {"coke": {"price": 120, "num": 5}}
 
-    def insert(self, money):
+    def insert(self, money: List[int]) -> List[int]:
+
         invalid_money = []
-        if type(money) == int:
-            if not validate_money(money):
-                invalid_money.append(money)
+        for m in money:
+            if not self.validate_money(m):
+                invalid_money.append(m)
             else:
-                self.money_box[money] += 1
-        elif type(money) == list:
-            for m in money:
-                validate_money(m)
                 self.money_box[m] += 1
 
         return invalid_money
 
-        # TODO: listとint以外も考慮する
+    def validate_money(self, money) -> bool:
+        return money in self.money_box.keys()
 
-    def validate_money(self, money):
-
-        
     # TODO: コメントが足りない！
-    def get_amount(self):
+    def get_amount(self) -> int:
         return sum([k * v for k, v in self.money_box.items()])
 
-    # 返ってくるのdictってわかりづらい？
-
-    def refund_money(self):
+    def refund_money(self) -> Dict[int, int]:
         money_box = self.money_box
         self.money_box = {k: 0 for k in self.money_box.keys()}
         return money_box
+
+    def get_juice_info(self) -> Dict[str, Dict]:
+        return self.juice_box
 
