bcde8b72c10e6da629b01ba644adbc5d078d3e0e
kttaroha <kthamano1994@gmail.com>
2020-10-01 13:17:05 +0900

    add some modifications

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -1,12 +1,12 @@
 from vending_machine.vending_machine import VendingMachine
 
 
-def test_check_coins():
+def test_check_money():
     vending_machine = VendingMachine()
-    insert_coins = 10
-    vending_machine.insert(insert_coins)
+    insert_money = 10
+    vending_machine.insert(insert_money)
 
-    actual = vending_machine.coins
+    actual = vending_machine.money_box
     expected = {10: 1, 50: 0, 100: 0, 500: 0, 1000: 0}
 
     assert actual == expected
@@ -14,10 +14,47 @@ def test_check_coins():
 
 def test_check_multiple_insert():
     vending_machine = VendingMachine()
-    insert_coins = [10, 50, 100, 500, 1000]
-    vending_machine.insert(insert_coins)
+    insert_money = [10, 50, 100, 500, 1000]
+    vending_machine.insert(insert_money)
 
-    actual = vending_machine.coins
+    actual = vending_machine.money_box
     expected = {10: 1, 50: 1, 100: 1, 500: 1, 1000: 1}
 
     assert actual == expected
+
+
+def test_get_amount():
+    vending_machine = VendingMachine()
+    insert_money = [10, 50, 100, 500, 1000]
+    vending_machine.insert(insert_money)
+
+    actual = vending_machine.get_amount()
+    expected = sum(insert_money)
+
+    assert actual == expected
+
+
+def test_refund_money():
+    vending_machine = VendingMachine()
+    insert_money = [10, 50, 100, 500, 1000]
+    vending_machine.insert(insert_money)
+
+    actual_refunded_money = vending_machine.refund_money()
+    expected_refunded_money = {10: 1, 50: 1, 100: 1, 500: 1, 1000: 1}
+
+    assert actual_refunded_money == expected_refunded_money
+
+    actual_amount = vending_machine.get_amount()
+    expected_amount = 0
+
+    assert actual_amount == expected_amount
+
+def test_insert_invalid_money():
+    vending_machine = VendingMachine()
+    insert_money = [1, 5, 10000, 3]
+    
+    actual = vending_machine.insert(insert_money)
+    expected = insert_money
+    assert actual == expected
+
+
--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,10 +1,34 @@
 class VendingMachine:
     def __init__(self):
-        self.coins = {10: 0, 50: 0, 100: 0, 500: 0, 1000: 0}
+        self.money_box = {10: 0, 50: 0, 100: 0, 500: 0, 1000: 0}
 
-    def insert(self, coin):
-        if type(coin) == int:
-            self.coins[coin] += 1
-        elif type(coin) == list:
-            for c in coin:
-                self.coins[c] += 1
+    def insert(self, money):
+        invalid_money = []
+        if type(money) == int:
+            if not validate_money(money):
+                invalid_money.append(money)
+            else:
+                self.money_box[money] += 1
+        elif type(money) == list:
+            for m in money:
+                validate_money(m)
+                self.money_box[m] += 1
+
+        return invalid_money
+
+        # TODO: listとint以外も考慮する
+
+    def validate_money(self, money):
+
+        
+    # TODO: コメントが足りない！
+    def get_amount(self):
+        return sum([k * v for k, v in self.money_box.items()])
+
+    # 返ってくるのdictってわかりづらい？
+
+    def refund_money(self):
+        money_box = self.money_box
+        self.money_box = {k: 0 for k in self.money_box.keys()}
+        return money_box
+
